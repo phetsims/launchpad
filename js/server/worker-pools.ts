5818afc3c3b0a98bdd4f73086d2c8ac66146ad58
Jonathan Olson <jonathan.olson@colorado.edu>
2025-11-12 21:08:47 -0700

    worker pool cleanup, and fixing missing modulify worker logging

--- js/server/worker-pools.ts
+++ js/server/worker-pools.ts
@@ -12,62 +12,6 @@ import { logger } from './logging.js';
 import { QueryParameter, Repo, SHA } from '../types/common-types.js';
 import { modulifySAB } from './workers/modulifyGlobalReset.js';
 
-export const bundlePool = new Piscina<{ filePath: string; modulify: boolean }, string>( {
-  filename: new URL( './workers/entry-points/bundle.js', import.meta.url ).href,
-  minThreads: 1,
-  maxThreads: 8,
-  idleTimeout: 60 * 60 * 1000,
-  workerData: {
-    ROOT_DIR: ROOT_DIR,
-    cacheModulification: cacheModulification,
-    modulifySAB: modulifySAB
-  }
-} );
-export const transpilePool = new Piscina<string | { filePath: string; contents: string }, string>( {
-  filename: new URL( './workers/entry-points/transpile.js', import.meta.url ).href,
-  minThreads: 1,
-  maxThreads: 8,
-  idleTimeout: 60 * 60 * 1000,
-  workerData: {
-    ROOT_DIR: ROOT_DIR,
-    cacheModulification: cacheModulification,
-    modulifySAB: modulifySAB
-  }
-} );
-export const modulifyPool = new Piscina<{ relativePath: string; chipperSHA: SHA; perennialSHA: SHA }, string | null>( {
-  filename: new URL( './workers/entry-points/modulify.js', import.meta.url ).href,
-  minThreads: 1,
-  maxThreads: 8,
-  idleTimeout: 60 * 60 * 1000,
-  workerData: {
-    ROOT_DIR: ROOT_DIR,
-    cacheModulification: cacheModulification,
-    modulifySAB: modulifySAB
-  }
-} );
-export const getStrongEtagPool = new Piscina<string, string>( {
-  filename: new URL( './workers/entry-points/strong-etag.js', import.meta.url ).href,
-  minThreads: 1,
-  maxThreads: 8,
-  idleTimeout: 60 * 60 * 1000,
-  workerData: {
-    ROOT_DIR: ROOT_DIR,
-    cacheModulification: cacheModulification,
-    modulifySAB: modulifySAB
-  }
-} );
-export const getExtractQueryParametersPool = new Piscina<{ repo: Repo; directory: string }, QueryParameter[]>( {
-  filename: new URL( './workers/entry-points/extract-query-parameters.js', import.meta.url ).href,
-  minThreads: 1,
-  maxThreads: 8,
-  idleTimeout: 60 * 60 * 1000,
-  workerData: {
-    ROOT_DIR: ROOT_DIR,
-    cacheModulification: cacheModulification,
-    modulifySAB: modulifySAB
-  }
-} );
-
 const attachLogging = ( pool: Piscina, name: string ) => {
   pool.on( 'message', ( message: { logLevel: string; message: string } ) => {
     if ( message.logLevel ) {
@@ -77,7 +21,40 @@ const attachLogging = ( pool: Piscina, name: string ) => {
   } );
 };
 
-attachLogging( bundlePool, 'bundle' );
-attachLogging( transpilePool, 'transpile' );
-attachLogging( getStrongEtagPool, 'strong-etag' );
-attachLogging( getExtractQueryParametersPool, 'extract-query-parameters' );
+const createPool = <T, U>( name: string, entryPoint: string ): Piscina<T, U> => {
+  const pool = new Piscina<T, U>( {
+    filename: new URL( entryPoint, import.meta.url ).href,
+    minThreads: 1,
+    maxThreads: 8,
+    idleTimeout: 60 * 60 * 1000,
+    workerData: {
+      ROOT_DIR: ROOT_DIR,
+      cacheModulification: cacheModulification,
+      modulifySAB: modulifySAB
+    }
+  } );
+  attachLogging( pool, name );
+
+  return pool;
+};
+
+export const bundlePool = createPool<{ filePath: string; modulify: boolean }, string>(
+  'bundle',
+  './workers/entry-points/bundle.js'
+);
+export const transpilePool = createPool<string | { filePath: string; contents: string }, string>(
+  'transpile',
+  './workers/entry-points/transpile.js'
+);
+export const modulifyPool = createPool<{ relativePath: string; chipperSHA: SHA; perennialSHA: SHA }, string | null>(
+  'modulify',
+  './workers/entry-points/modulify.js'
+);
+export const getStrongEtagPool = createPool<string, string>(
+  'strong-etag',
+  './workers/entry-points/strong-etag.js'
+);
+export const getExtractQueryParametersPool = createPool<{ repo: Repo; directory: string }, QueryParameter[]>(
+  'extract-query-parameters',
+  './workers/entry-points/extract-query-parameters.js'
+);
